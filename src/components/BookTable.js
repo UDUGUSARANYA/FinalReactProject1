060278a62bfebfb9b43085040172ec8e9e8c639d
Udugu-Saranya <saranyaudugu@gmail.com>
2026-02-02 15:24:57 +0530

    Fix ESLint errors for Vercel build

--- src/components/BookTable.js
+++ src/components/BookTable.js
@@ -16,9 +16,16 @@ const BookTable = () => {
                 </form>
             </div>
             <div className="map-container">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13773.555445861073!2d78.39559795!3d17.5042406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1769873059136!5m2!1sen!2sin"
-                 width="100%" height="100%" allowfullscreen="" aria-hidden="false" tabIndex="0" >
-                </iframe>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13773.555445861073!2d78.39559795!3d17.5042406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sin!4v1769873059136!5m2!1sen!2sin"
+                    width="100%"
+                    height="100%"
+                    title="Restaurant location map"
+                    allowFullScreen
+                    aria-hidden="false"
+                    tabIndex="0"
+                ></iframe>
+
               
             
               
