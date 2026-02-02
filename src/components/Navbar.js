33cf7db35ebda094fdcf0865d3c502e6dcf790a8
Udugu-Saranya <saranyaudugu@gmail.com>
2026-02-02 15:29:31 +0530

    Remove unused framer-motion import

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,79 @@
-import React,{useState} from 'react';
-import './Navbar.css';
-import {FaUser, FaShoppingCart, FaSearch, FaBars, FaTimes} from 'react-icons/fa';
-import { nav } from 'framer-motion/client';
+// import React,{useState} from 'react';
+// import './Navbar.css';
+// import {FaUser, FaShoppingCart, FaSearch, FaBars, FaTimes} from 'react-icons/fa';
+// import { nav } from 'framer-motion/client';
 
-const Navbar = () => {
+// const Navbar = () => {
 
 
-    const[isMobile, setIsMobile] = useState(false);
+//     const[isMobile, setIsMobile] = useState(false);
 
-    return (
-        <nav className='Navbar'>
-            <h1 className='navbar-logo'>Feane</h1>
-            <div className={isMobile ? 'nav-links-mobile' : 'nav-links'}>
-                <a href='home'>Home</a>
-                <a href='about'>About</a>
-                <a href='menu'>Menu</a>
-                <a href='book-table'>Book Table</a>
-                <FaUser className='nav-icon'></FaUser>
-                <FaShoppingCart className='nav-icon'></FaShoppingCart>
-                <FaSearch className='nav-icon'></FaSearch>
-                <button className='orders-button'>Order Online</button>
+//     return (
+//         <nav className='Navbar'>
+//             <h1 className='navbar-logo'>Feane</h1>
+//             <div className={isMobile ? 'nav-links-mobile' : 'nav-links'}>
+//                 <a href='home'>Home</a>
+//                 <a href='about'>About</a>
+//                 <a href='menu'>Menu</a>
+//                 <a href='book-table'>Book Table</a>
+//                 <FaUser className='nav-icon'></FaUser>
+//                 <FaShoppingCart className='nav-icon'></FaShoppingCart>
+//                 <FaSearch className='nav-icon'></FaSearch>
+//                 <button className='orders-button'>Order Online</button>
             
 
-            </div>
-            <div className='mobile-menu-icon' onClick={() => setIsMobile(!isMobile)}>
-                {isMobile ? <FaTimes /> : <FaBars />}
+//             </div>
+//             <div className='mobile-menu-icon' onClick={() => setIsMobile(!isMobile)}>
+//                 {isMobile ? <FaTimes /> : <FaBars />}
                 
-            </div>
-        </nav>
-    )
-}
+//             </div>
+//         </nav>
+//     )
+// }
+// export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+import './Navbar.css';
+import { FaUser, FaShoppingCart, FaSearch, FaBars, FaTimes } from 'react-icons/fa';
+
+const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <nav className='Navbar'>
+      <h1 className='navbar-logo'>Feane</h1>
+
+      <div className={isMobile ? 'nav-links-mobile' : 'nav-links'}>
+        <a href='home'>Home</a>
+        <a href='about'>About</a>
+        <a href='menu'>Menu</a>
+        <a href='book-table'>Book Table</a>
+
+        <FaUser className='nav-icon' />
+        <FaShoppingCart className='nav-icon' />
+        <FaSearch className='nav-icon' />
+
+        <button className='orders-button'>Order Online</button>
+      </div>
+
+      <div
+        className='mobile-menu-icon'
+        onClick={() => setIsMobile(!isMobile)}
+      >
+        {isMobile ? <FaTimes /> : <FaBars />}
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
