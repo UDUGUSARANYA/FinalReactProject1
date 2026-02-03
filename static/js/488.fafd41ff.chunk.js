6a4ff05bc62fb7f8a1a0f272942d692a1454b4c7
Udugu-Saranya <saranyaudugu@gmail.com>
2026-02-03 21:28:00 +0530

    Updates

---
+++ static/js/488.fafd41ff.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_fast_food_restaurant=self.webpackChunkmy_fast_food_restaurant||[]).push([[488],{488(e,a,s){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.fafd41ff.chunk.js.map
